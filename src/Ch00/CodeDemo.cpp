bc4e58ca13b4b816ab1a066dbf07e533a5039b6d
siddiquaaly <alysid786@gmail.com>
2025-09-25 06:03:59 +0000

    Modified the code to add numbers

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03
+// Exercise 00_03,Good work Keep going
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
@@ -7,9 +7,9 @@
 int main(){
     float num_1, num_2, result;
 
-    std::cout << "Enter number 1: " << std::flush;
+    std::cout << "Enter number 1: " << std::endl;
     std::cin >> num_1;
-    std::cout << "Enter number 2: " << std::flush;
+    std::cout << "Enter number 2: " << std::endl;
     std::cin >> num_2;
     
     result = num_1 + num_2;
